92275b23b03a1fdc457d2e8c1374c1bb70bbefc3
Taky0nDon <126304313+Taky0nDon@users.noreply.github.com>
2023-07-13 04:57:00 -0400

    working prototype
    more attempts at inserting cookies. it works now but breaks when it encounters a match

--- day50readme.md
+++ day50readme.md
@@ -106,9 +106,20 @@ driver.get("https://www.website.com")
 
 ```
 
+## EXPECTED CONDITIONS
+* locator is a tuple: `By, Path` (`By.ID, "some_id"`)
+
+## Finding an element by attribute value:
+By.CSS_SELECTOR, tag_name[attribute="value"]
+
 ## ANALYZE THE BROWSER FINGERPRINT
 * [Cover Your Tracks](https://wwww.coveryourtracks.eff)
 
 * [Common Bot Detection Techniques](https://github.com/0xInfection/Awesome-WAF)
 
 * [A Great Stack Overflow Question About Selenium Detection](https://stackoverflow.com/questions/33225947/can-a-website-detect-when-you-are-using-selenium-with-chromedriver)
+
+## CLICK INTERCEPTED
+
+selenium.common.exceptions.ElementClickInterceptedException: Message: element click intercepted: Element `<div class="Pos(a) P(.1em) Pe(n) Fw($semibold) Tt(u) Bdrs(4px) Bdw(4px) Bds(s) Wc($opacity) Op(0) Trsp($opacity) Trsdu($fast) Fz(4rem) Lh(3.5rem) Rotate(-20deg) T(10%) Start(10%) C($c-like-green) Bdc($c-like-green)" style="opacity: 0;">...</div>` 
+is not clickable at point (584, 118). Other element would receive the click: `<div role="img" class="Bdrs(8px) Bgz(cv) Bgp(c) StretchedBox" aria-labelledby="2d3d3044-d1fe-4744-ae8b-1ea9ef9ff81b" style="background-image: url(&quot;https://images-ssl.gotinder.com/u/9ijXcQmgQ2sPcDBVciq2Uf/uJBrjz668CpL7XN7dxbFQz.webp?Policy=eyJTdGF0ZW1lbnQiOiBbeyJSZXNvdXJjZSI6IiovdS85aWpYY1FtZ1Eyc1BjREJWY2lxMlVmLyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE2ODk1NDIzOTh9fX1dfQ__&amp;Signature=i7PWHVREyFPnA-E8V4uaa8YgA7X7RkhBO5eKneICx8oc0AGbLU3~tl4u127KuOrAQ2~jdbOpnWUdT0Wjnbqlg9GtEqDYfkonG56EF49RmqyY6fqN373TiyFFXUo5ML~RJw8pdz6VCZqIjiaYNKWisIqJ0QB-IzgbYUixg7Ln9K~k6H9xAG1iAk9f4uHoXpKbYxA13kny~mtVnvjDWPUEim9-xLUW48n6IN-8XK25k8lXmlhXbsliAOq9p0ewrGEIps2ms5G-mRPVI1-8mj1KFPTsPprN7FjJIvcVxdy1bD50Vxvt0HpwS7zz20f4RpElsHD4vFCSRbRJJKvfgkx0bA__&amp;Key-Pair-Id=K368TLDEUPA6OI&quot;); background-position: 50% 0%; background-size: 110.256%;"></div>`
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import os
+import json
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -8,12 +9,14 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+import selenium_stealth
+
 EMAIL = os.environ.get("EMAIL")
 PASS = os.environ.get("PASS")
 
 
 url = "https://tinder.com"
-decline_cookies_xpath = '//*[@id="u-1238065328"]/main/div[2]/div/div/div[1]/div[2]/button/div[2]/div[2]'
+decline_cookies_xpath = '//*[@id="t223514671"]/main/div[2]/div/div/div[1]/div[2]/button/div[2]/div[2]'
 next_xpath = '//*[@id="identifierNext"]/div/button/span'
 
 
@@ -27,9 +30,26 @@ options.add_experimental_option("useAutomationExtension", False)
 # stay_open.add_argument("--remote-debugging-port=9222")
 driver = webdriver.Chrome(options)
 driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+# driver.maximize_window()
+selenium_stealth.stealth(driver,
+                         user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+                         languages=["en","en-US"],
+                         platform="web",
+                         )
 driver.get("https://tinder.com/404")
 time.sleep(1)
+### ADDING COOKIES
+with open("cookies.json") as file:
+    lines = file.readlines()
+    cookie_list = []
+    for line in lines:
+        print(line)
+        cookie_list.append(json.loads(line))
+    for cookie in cookie_list:
+        driver.add_cookie(cookie)
 
+
+print("here")
 driver.get(url)
 print(driver.current_window_handle)
 time.sleep(3)
@@ -53,20 +73,32 @@ print(driver.current_window_handle)
 
 driver.find_element(By.TAG_NAME, "input").send_keys(EMAIL)
 driver.find_element(By.XPATH, next_xpath).click()
-time.sleep(1)
-driver.find_element(By.XPATH, '//*[@id="password"]/div[1]/div/div[1]/input').send_keys(PASS, Keys.ENTER)
+time.sleep(5)
+# wait.until(EC.presence_of_element_located(locator=(By.ID, "password")))
+password_input = driver.find_element(By.XPATH, '//*[@id="password"]/div[1]/div/div[1]/input')
+password_input.send_keys(PASS, Keys.ENTER)
 time.sleep(2)
 keep_going = input("Hit ENTER to continue.")
 driver.switch_to.window(original_window)
+with open("tindercookies.txt", "w") as file:
+    cookies = driver.get_cookies()
+    for cookie in cookies:
+        json.dump(cookie, file)
 # Allow location
-driver.find_element(By.XPATH, '//*[@id="u-1238065328"]/main/div/div/div/div[3]/button[1]/div[2]/div[2]').click()
+driver.find_element(By.CSS_SELECTOR, 'button[aria-label="Allow"]').click()
 #Don't allow notifications
-driver.find_element(By.XPATH, '//*[@id="u-1238065328"]/main/div/div/div/div[3]/button[2]/div[2]/div[2]').click()
+driver.find_element(By.CSS_SELECTOR, 'button[aria-label="Not interested"]').click()
 time.sleep(5)
 # Start smashing those hearts
+just_before_like_cookies = driver.get_cookies()
+for cookie in just_before_like_cookies:
+    with open("cookies.json", "a") as file:
+        json.dump(cookie, file)
 for n in range(10):
-    driver.find_element(By.XPATH,
-                        '//*[@id="u490315748"]/div/div[1]/div/div/main/div/div/div[1]/div/div[3]/div/div[4]/button/'
-                        'span/span/svg/path')\
-        .click()
+    # child = driver.find_element(By.XPATH, '//*[text()="Like"]')
+    # parent = child.find_element(By.XPATH, "./..")
+    child = driver.find_element(By.CLASS_NAME, 'Bdc\(\$c-ds-border-gamepad-like-default\)')
+    child.click()
+    print("Liked!")
     time.sleep(1)
+# Need to account for matches!
